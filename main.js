5653e8bf3967b59c65880f4630b74569f45e2f5f
Bradderz65 <bradjhosk@outlook.com>
2023-05-12 23:23:12 +0100

    Initial commit

--- main.js
+++ main.js
@@ -10,12 +10,27 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    // Load existing tasks from localStorage
+    loadTasks();
+
     function addTask() {
         const taskText = taskInput.value.trim();
         if (!taskText) return;
 
+        const taskId = Date.now(); // Generate a unique task ID based on the current timestamp
+        
+        const taskItem = createTaskItem(taskId, taskText);
+        taskList.appendChild(taskItem);
+        taskInput.value = "";
+
+        // Save the new task to localStorage
+        saveTask(taskId, taskText);
+    }
+
+    function createTaskItem(taskId, taskText) {
         const taskItem = document.createElement("div");
         taskItem.classList.add("task-item");
+        taskItem.dataset.taskId = taskId;
 
         const taskCheckbox = document.createElement("input");
         taskCheckbox.type = "checkbox";
@@ -30,13 +45,35 @@ document.addEventListener("DOMContentLoaded", () => {
         taskRemove.textContent = "Remove";
         taskRemove.addEventListener("click", () => {
             taskList.removeChild(taskItem);
+            // Remove the task from localStorage
+            removeTask(taskId);
         });
 
         taskItem.appendChild(taskCheckbox);
         taskItem.appendChild(taskLabel);
         taskItem.appendChild(taskRemove);
 
-        taskList.appendChild(taskItem);
-        taskInput.value = "";
+        return taskItem;
+    }
+
+    function saveTask(taskId, taskText) {
+        const tasks = JSON.parse(localStorage.getItem("tasks")) || {};
+        tasks[taskId] = taskText;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+    function removeTask(taskId) {
+        const tasks = JSON.parse(localStorage.getItem("tasks")) || {};
+        delete tasks[taskId];
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+    function loadTasks() {
+        const tasks = JSON.parse(localStorage.getItem("tasks")) || {};
+        for (const taskId in tasks) {
+            const taskText = tasks[taskId];
+            const taskItem = createTaskItem(taskId, taskText);
+            taskList.appendChild(taskItem);
+        }
     }
 });
